3003a57332a650af539ee8fd3acef18bb633ac2d
0110125102-hqs <0110125102@student.nurulfikri.ac.id>
2025-11-20 23:42:01 +0700

    Add files via upload

---
+++ P9 Soal No3.py
@@ -0,0 +1,9 @@
+def nilai(n=0):
+    if n <= 60:
+        print(f"nilai {n} Tidak Lulus")
+    elif n > 60 and n <= 100:
+        print(f"nilai {n} Lulus")
+    else:
+        print(f"nilai {n} Tidak Diketahui")
+nilai(80)
+nilai(60)
